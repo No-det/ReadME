c31b58a66927abe93f4cca2cdf5ad8c43f3f19c4
Abhinav Rajesh <abhinavrajesh49@gmail.com>
2021-05-17 22:37:42 +0530

    Added some dependencies to server

--- server/app.js
+++ server/app.js
@@ -1,13 +1,44 @@
 const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
+require("dotenv").config();
+
+const apiRoutes = require("./routes/api.routes");
 
 const app = express();
-const PORT = 5000 || process.env.PORT;
+const PORT = process.env.PORT || 5000;
+
+// Mongodb connection
+mongoose
+  .connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+  })
+  .then(
+    (db) => {
+      console.log("Connected to the DB");
+    },
+    (err) => {
+      console.log(err);
+    }
+  );
+
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+    credentials: true,
+  })
+);
+app.use(express.json());
 
 app.get("/", (req, res) => {
   res.status(200).json({
     message: "API Working",
   });
 });
+
+app.use("/api/v1", apiRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server listening at *:${PORT}`);
