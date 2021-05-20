b3e65adce4128ce6b264b5d5eb6f248bebf647a1
Abhinav Rajesh <abhinavrajesh49@gmail.com>
2021-05-20 13:38:31 +0530

    Added disqus to the project

--- client/src/pages/Review/index.js
+++ client/src/pages/Review/index.js
@@ -1,24 +1,15 @@
-import { Comment, message, Avatar, Form, Input, Button } from "antd";
-import { useContext, useEffect, useState } from "react";
+import { message } from "antd";
+import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
+import { DiscussionEmbed } from "disqus-react";
 
-import { addComment, getReview } from "../../api/review";
-import { AuthContext } from "../../contexts/AuthContext";
-
-import CommentList from "../../components/CommentList";
+import { getReview } from "../../api/review";
 
 import "./index.scss";
 
-const { TextArea } = Input;
-
 const Review = (props) => {
-  const [submitting, setSubmitting] = useState(false);
   const [review, setReview] = useState({});
   const [invalidRoute, setInvalidRoute] = useState(false);
-  const [value, setValue] = useState("");
-  const [comments, setComments] = useState([]);
-
-  const { user } = useContext(AuthContext);
 
   useEffect(() => {
     const asyncFunction = async () => {
@@ -36,48 +27,6 @@ const Review = (props) => {
     };
     asyncFunction();
   }, []);
-
-  const Editor = ({ onChange, onSubmit, submitting, value }) => (
-    <>
-      <Form.Item>
-        <TextArea rows={4} onChange={onChange} value={value} />
-      </Form.Item>
-      <Form.Item>
-        <Button
-          htmlType="submit"
-          loading={submitting}
-          onClick={onSubmit}
-          type="primary"
-        >
-          Add Comment
-        </Button>
-      </Form.Item>
-    </>
-  );
-
-  const handleSubmit = () => {
-    if (!value) return;
-    setSubmitting(true);
-    const payload = {
-      uid: user.uid,
-      email: user.email,
-      displayName: user.displayName,
-      comment: value,
-      upvote: 0,
-    };
-    console.log(payload);
-    // addComment(props?.match?.params?.id, payload)
-    //   .then((res) => {
-    //     if (res.success) {
-    //       setComments(res.comments);
-    //     }
-    //   })
-    //   .catch((err) => {
-    //     console.log(err);
-    //     message.error("Some error occured! Please try again later");
-    //   });
-    setSubmitting(false);
-  };
 
   return (
     <div>
@@ -129,23 +78,15 @@ const Review = (props) => {
             </div>
           </div>
           <div className="reviewComments">
-            <h3 className="comments">
-              <span className="numberOfComments">{comments.length}</span>{" "}
-              Comments
-            </h3>
-            {comments.length > 0 &&
-              comments.map((comment) => <CommentList comment={comment} />)}
-            <Comment
-              avatar={<Avatar src={user?.photoURL} alt={user?.displayName} />}
-              content={
-                <Editor
-                  onChange={(e) => setValue(e.target.value)}
-                  onSubmit={handleSubmit}
-                  submitting={submitting}
-                  value={value}
-                />
-              }
-            />
+            <div className="commentSection">
+              <DiscussionEmbed
+                shortname="readme-3"
+                config={{
+                  identifier: props?.match?.params?.id,
+                  title: props?.match?.params?.id,
+                }}
+              />
+            </div>
           </div>
         </div>
       )}
