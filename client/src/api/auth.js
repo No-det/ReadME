99d2b0a7f08279d96f3e04781a67468157df7521
Nandan-unni <asnqln@gmail.com>
2021-05-21 15:01:10 +0530

    added update profile

--- client/src/api/auth.js
+++ client/src/api/auth.js
@@ -3,3 +3,7 @@ import { post } from "./base";
 export const addUser = (user) => {
   return post("/user/add", user);
 };
+
+export const updateUser = (data) => {
+  return post("/user/update", data);
+};
--- client/src/pages/Profile/index.js
+++ client/src/pages/Profile/index.js
@@ -1,17 +1,67 @@
-import { useContext } from "react";
-import { Tabs } from "antd";
+import { useContext, useState } from "react";
+import { Tabs, Drawer, Form, Input, Button, message } from "antd";
 
 import { AuthContext } from "../../contexts/AuthContext";
 import ProfileReviews from "../../components/ProfileReviews";
 import ProfileTrades from "../../components/ProfileTrades";
 import "./index.scss";
 
+import { updateUser } from "../../api/auth";
+
+import whatsappIcon from "../../assets/whatsapp.svg";
+import telegramIcon from "../../assets/telegram.svg";
+import twitterIcon from "../../assets/twitter.svg";
+import facebookIcon from "../../assets/facebook.svg";
+import defaultIcon from "../../assets/socialMedia.svg";
+import instagramIcon from "../../assets/instagram.svg";
+
 const { TabPane } = Tabs;
 
 const Profile = () => {
   const { user } = useContext(AuthContext);
+  const [isEditing, setIsEditing] = useState(false);
+  const [isSubmitting, setIsSubmitting] = useState(false);
+  const [connectIcon, setConnectIcon] = useState(defaultIcon);
+  const [form] = Form.useForm();
 
   console.log(user);
+
+  const handleMediaChange = (e) => {
+    let value = e.target.value;
+    if (value.includes("facebook")) setConnectIcon(facebookIcon);
+    else if (value.includes("twitter")) setConnectIcon(twitterIcon);
+    else if (value.includes("telegram")) setConnectIcon(telegramIcon);
+    else if (value.includes("instagram")) setConnectIcon(instagramIcon);
+    else if (value.includes("whatsapp")) setConnectIcon(whatsappIcon);
+    else setConnectIcon(defaultIcon);
+  };
+
+  const handleUpdateProfile = async (data) => {
+    if (data.displayName && data.bio && data.connect) {
+      setIsSubmitting(true);
+      const payload = {
+        displayName: data.displayName,
+        bio: data.bio,
+        connect: data.connect,
+      };
+      updateUser(payload)
+        .then((data) => {
+          console.log(data);
+          if (data?.success) {
+            message.success(data.message);
+            form.resetFields();
+          }
+        })
+        .catch((err) => {
+          console.log(err);
+          message.error(err.toString());
+        });
+      setIsSubmitting(false);
+      setIsEditing(false);
+    } else {
+      message.error("Please fill out all the fields !");
+    }
+  };
 
   return (
     <div className="profileContainer">
@@ -35,9 +85,12 @@ const Profile = () => {
       </div>
       <div className="profileContentWrapper">
         <div className="profileContent">
-          <h2>{user?.displayName}</h2>
-          <small>{user?.email}</small>
-          <p>{user?.bio}</p>
+          <span>
+            <h2>{user?.displayName}</h2>
+            <small>{user?.email}</small>
+            <p>{user?.bio}</p>
+          </span>
+          <button onClick={() => setIsEditing(true)}>Edit Profile</button>
         </div>
         <div className="mobileFollowContainer">
           <p>
@@ -58,6 +111,56 @@ const Profile = () => {
           </TabPane>
         </Tabs>
       </div>
+
+      <Drawer
+        visible={isEditing}
+        onClose={() => setIsEditing(false)}
+        width={window.innerWidth > 500 ? 500 : 300}
+        title="Edit Profile"
+      >
+        <Form
+          layout="vertical"
+          onFinish={handleUpdateProfile}
+          initialValues={{
+            displayName: user?.displayName,
+            bio: user?.bio,
+            connect: user?.connect,
+          }}
+        >
+          <Form.Item label="Name" name="displayName">
+            <Input value={user?.displayName} />
+          </Form.Item>
+          <Form.Item label="Bio" name="bio">
+            <Input.TextArea
+              value={user?.bio}
+              showCount
+              autoSize={{ minRows: 3, maxRows: 5 }}
+              maxLength={200}
+            />
+          </Form.Item>
+          <Form.Item
+            name="connect"
+            label="Social Media Link"
+            rules={[{ type: "url", message: "Please enter a valid URL" }]}
+          >
+            <Input
+              value={user?.connect}
+              onChange={(e) => handleMediaChange(e)}
+              addonBefore={
+                <img src={connectIcon} className="connectIcon" alt="icon" />
+              }
+            />
+          </Form.Item>
+          <Button
+            size="large"
+            htmlType="submit"
+            type="primary"
+            loading={isSubmitting}
+          >
+            Post
+          </Button>
+        </Form>
+      </Drawer>
     </div>
   );
 };
