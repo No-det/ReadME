b8f0f00a7a2bff8e5635d087e5058c4a14692b96
Ajal <ajapoos1999@gmai.com>
2021-05-23 12:34:35 +0530

    search for chat is done

--- client/src/pages/ChatPage/index.js
+++ client/src/pages/ChatPage/index.js
@@ -4,26 +4,36 @@ import ChatRoom from "../../components/ChatRoom";
 import Search from "../../assets/search.svg";
 import { useContext, useState } from "react";
 import { AuthContext } from "../../contexts/AuthContext";
+import { Input } from "antd";
 
 const ChatPage = () => {
   const { user } = useContext(AuthContext);
   const [selectedReceiver, setSelectedReceiver] = useState({});
+  const [search, setSearch] = useState("");
+
   return (
     <div className="chatPage">
       <div className="chatSelect">
         <div className="chatSearch">
-          <input />
+          <Input
+            placeholder="Enter the name."
+            onChange={({ target }) => setSearch(target.value)}
+          />
           <img src={Search} alt="search" />
         </div>
         <div className="chatListWrapper">
           <div className="chatList">
             {user?.following?.length > 0 ? (
-              user?.following?.map((doc) => (
-                <ChatTile
-                  onClick={() => setSelectedReceiver(doc)}
-                  userData={doc}
-                />
-              ))
+              user?.following
+                ?.filter((follower) =>
+                  follower.name.toLowerCase().includes(search.toLowerCase())
+                )
+                .map((doc) => (
+                  <ChatTile
+                    onClick={() => setSelectedReceiver(doc)}
+                    userData={doc}
+                  />
+                ))
             ) : (
               <p className="chatEmptyMessage">
                 Follow some users to start a conversation
